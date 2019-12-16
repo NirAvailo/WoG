9c2ca746fa434e5548393986d36ec04314e4e579
niravailo <nir@shieldox.com>
2019-12-16 20:21:24 +0200

    first commit

---
+++ MainGame.py
@@ -0,0 +1,14 @@
+from Live import load_game, welcome
+# World of Games Starting point
+# Note:
+# It better to use it as entry point so it won't run on import
+
+
+def main():
+    print(welcome("Player"))
+    load_game()
+
+
+if __name__ == '__main__':
+    main()
+1
